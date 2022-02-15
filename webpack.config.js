7087c5f055d963665fff4500c4053143b4eb92da
Kannan Nagasamy <kannann@INLM-KannanN-1.local>
2022-02-15 16:41:33 +0530

    Code changes made to match with documentation

--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,67 @@ const path = require("path");
 After that apply all the rules in module.rules and produce the output and place it in main.js in the public folder.*/
 
 module.exports={
-    mode: "development", //the environment - development, production, none. tells webpack to use its built-in optimizations accordingly. default is production
-    entry: "./index.js", //the entry point
+    /** "mode"
+     * the environment - development, production, none. tells webpack 
+     * to use its built-in optimizations accordingly. default is production 
+     */
+    mode: "development", 
+    /** "entry"
+     * the entry point 
+     */
+    entry: "./index.js", 
     output: {
-        path: path.resolve(__dirname, "public"), //the folder path of the output file
-        filename: "main.js" //the name of the output file
+        /** "path"
+         * the folder path of the output file 
+         */
+        path: path.resolve(__dirname, "public"),
+        /** "filename"
+         * the name of the output file 
+         */
+        filename: "main.js"
     },
-    target: "web", //setting "node" as target app (server side), and setting it as "web" is for browser (client side). Default is "web"
+    /** "target"
+     * setting "node" as target app (server side), and setting it as "web" is 
+     * for browser (client side). Default is "web"
+     */
+    target: "web",
     devServer: {
-        port: "9500", //port of dev server
-        static: ["./public"], //This property tells Webpack what static file it should serve
-        open: true, //opens the browser after server is successfully started
-        //hot: true , //enabling and disabling HMR. takes "true", "false" and "only". "only" is used if enable Hot Module Replacement without page refresh as a fallback in case of build failures
-        //liveReload: true //disable live reload on the browser. "hot" must be set to false for this to work
+        /** "port" 
+         * port of dev server
+        */
+        port: "9500",
+        /** "static" 
+         * This property tells Webpack what static file it should serve
+        */
+        static: ["./public"],
+        /** "open" 
+         * opens the browser after server is successfully started
+        */
+        open: true,
+        /** "hot"
+         * enabling and disabling HMR. takes "true", "false" and "only". 
+         * "only" is used if enable Hot Module Replacement without page 
+         * refresh as a fallback in case of build failures
+         */
+        hot: true ,
+        /** "liveReload"
+         * disable live reload on the browser. "hot" must be set to false for this to work
+        */
+        liveReload: true
     },
     resolve: {
-        /* If multiple files share the same name but have different extensions, webpack will resolve the one with the 
-        extension listed first in the array and skip the rest. which is what enables users to leave off the extension when importing*/
+        /** "extensions" 
+         * If multiple files share the same name but have different extensions, webpack will 
+         * resolve the one with the extension listed first in the array and skip the rest. 
+         * This is what enables users to leave off the extension when importing
+         */
         extensions: ['.js','.jsx','.json'] 
     },
     module:{
-        /**
-         * "Hey webpack compiler, when you come across a path that resolves to a '.js or .jsx' file inside of a require()/import statement, use the 
-         * babel-loader to transform it before you add it to the bundle. And in this process, kindly make sure to exclude node_modules folder from 
+        /** "rules"
+         * This says - "Hey webpack compiler, when you come across a path that resolves to a '.js or .jsx' 
+         * file inside of a require()/import statement, use the babel-loader to transform it before you 
+         * add it to the bundle. And in this process, kindly make sure to exclude node_modules folder from 
          * being searched"
          */
         rules: [
